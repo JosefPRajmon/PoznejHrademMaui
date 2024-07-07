4cf3302e9498d2df22643b7e1e2a5bf4eac8bcce
Josef Rajmon <rajmon.josef1999@itrajmon.cz>
2024-07-07 09:39:46 +0100

    Přidejte soubory projektu.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PoznejHrademMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
